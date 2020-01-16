a5930318c11318ac07f9f8ab2d37e7b8c0275f92
cristian-etchebarne <cristian.etchebarne@globallogic.com>
2020-01-16 00:24:23 -0300

    Todo app created

---
+++ src/Todo/Input/Input.svelte
@@ -0,0 +1,39 @@
+<script>
+	import { createEventDispatcher } from 'svelte';
+
+  export let task = '';
+  export let adds;
+
+  const dispatch = createEventDispatcher();
+
+  const add = () => dispatch('submit');
+  const remove = () => dispatch('remove', task);
+</script>
+
+<style lang="scss">
+
+.add-btn {
+  background-color: lightgreen;
+  color: white;
+}
+
+.remove-btn {
+  background-color: crimson;
+  color: white;
+  padding-right: 0.6em;
+  padding-left: 0.6em;
+}
+
+</style>
+
+<div>
+
+  {#if adds}
+    <input type="text" bind:value={task}>
+    <button on:click={add} disabled={!task} class="add-btn">+</button>
+    {:else}
+    <input type="text" bind:value={task} disabled>
+    <button on:click={remove} class="remove-btn">-</button>
+  {/if}
+
+</div>
---
+++ src/Todo/Todo.svelte
@@ -0,0 +1,36 @@
+<script>
+import Input from './Input/Input';
+
+let newTask;
+
+let removingElement = false;
+
+let addingElement = true;
+
+let tasks = [];
+
+const handleSubmit = () => {
+  tasks = [...tasks, newTask];
+}
+
+const handleRemove = (task) => {
+  tasks = tasks.filter(t => t !== task.detail);
+}
+
+</script>
+
+<style lang="scss">
+  /* your styles go here */
+</style>
+
+<div>
+
+<h2>TODO Svelte App</h2>
+
+{#each tasks as task}
+  <Input bind:task={task} bind:adds={removingElement} on:remove={handleRemove} />
+{/each}
+
+<Input bind:task={newTask} bind:adds={addingElement} on:submit={handleSubmit}/>
+
+</div>
